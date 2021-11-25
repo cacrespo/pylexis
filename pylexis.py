533548eaa6034ce1aa868d9c76234cf3f3c06368
Carlos A. Crespo <lvccrespo@gmail.com>
2021-11-25 20:39:19 -0300

    add grid

--- pylexis.py
+++ pylexis.py
@@ -12,5 +12,12 @@ def pylexis(year_start, year_end, age_start, age_end):
 
        ax.set(xlim=(year_start, year_end), xticks=range(year_start, year_end+1),
               ylim=(age_start, age_end), yticks=range(age_start, age_end+1))
-
+       
+       # grid
+       plt.grid()
+       for i in range(year_start, year_end):
+                     plt.axline( (i, age_start), (i + 1  , age_start + 1 ), linewidth=0.3, color='gray')
+                     plt.axline( (year_start, i - year_start), (year_start + 1, i - year_start + 1), linewidth=0.3, color='gray')
+                     
+       
        plt.show()
