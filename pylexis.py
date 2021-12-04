cfb68917b419ef65bf3a8ae8351baa29bd88c2b2
Carlos A. Crespo <lvccrespo@gmail.com>
2021-12-04 16:54:33 -0300

    fix indent

--- pylexis.py
+++ pylexis.py
@@ -2,22 +2,24 @@ import matplotlib.pyplot as plt
 
 
 def pylexis(year_start, year_end, age_start, age_end):
-       
-       # plot
-       fig, ax = plt.subplots()
+    """ Plot a basic Lexis Diagram """
 
-       plt.xlabel("Year")
-       plt.ylabel("Age")
-       plt.title("Lexis Diagram")
+    # plot
+    fig, ax = plt.subplots()
 
-       ax.set(xlim=(year_start, year_end), xticks=range(year_start, year_end+1),
-              ylim=(age_start, age_end), yticks=range(age_start, age_end+1))
-       
-       # grid
-       plt.grid()
-       for i in range(year_start, year_end):
-                     plt.axline( (i, age_start), (i + 1  , age_start + 1 ), linewidth=0.3, color='gray')
-                     plt.axline( (year_start, i - year_start), (year_start + 1, i - year_start + 1), linewidth=0.3, color='gray')
-                     
-       
-       plt.show()
+    plt.xlabel("Year")
+    plt.ylabel("Age")
+    plt.title("Lexis Diagram")
+
+    ax.set(xlim=(year_start, year_end), xticks=range(year_start, year_end+1),
+    ylim=(age_start, age_end), yticks=range(age_start, age_end+1))
+
+    # grid
+    plt.grid()
+
+    for i in range(year_start, year_end):
+        plt.axline( (i, age_start), (i + 1  , age_start + 1 ), linewidth=0.3, color='gray')
+        plt.axline( (year_start, i - year_start), (year_start + 1, i - year_start + 1), linewidth=0.3, color='gray')
+
+
+    plt.show()
