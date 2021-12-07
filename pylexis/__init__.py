0a911ddfdf85e0c78915ec4915ecded251d480e3
Carlos A. Crespo <lvccrespo@gmail.com>
2021-12-07 09:44:11 -0300

    pylexis class

--- pylexis/__init__.py
+++ pylexis/__init__.py
@@ -1,25 +1,36 @@
 import matplotlib.pyplot as plt
 
 
-def create(year_start, year_end, age_start, age_end):
-    """ Plot a basic Lexis Diagram """
+class diagram():
+    """ Basic Lexis Diagram """
 
-    # plot
-    fig, ax = plt.subplots()
+    def __init__(self, year_start, year_end, age_start, age_end):
+        self.year_start = year_start
+        self.year_end = year_end
+        self.age_start = age_start
+        self.age_end = age_end
 
-    plt.xlabel("Year")
-    plt.ylabel("Age")
-    plt.title("Lexis Diagram")
+    def titles(self, x_label="Year", y_label="Age", title="Lexis Diagram"):
+        self.xlabel = x_label
+        self.ylabel = y_label
+        self.title = title
 
-    ax.set(xlim=(year_start, year_end), xticks=range(year_start, year_end+1),
-    ylim=(age_start, age_end), yticks=range(age_start, age_end+1))
+    def plot(self):
+        fig, ax = plt.subplots()
 
-    # grid
-    plt.grid()
+        plt.xlabel(self.xlabel)
+        plt.ylabel(self.ylabel)
+        plt.title(self.title)
 
-    for i in range(year_start, year_end):
-        plt.axline( (i, age_start), (i + 1  , age_start + 1 ), linewidth=0.3, color='gray')
-        plt.axline( (year_start, i - year_start), (year_start + 1, i - year_start + 1), linewidth=0.3, color='gray')
+        ax.set(xlim=(self.year_start, self.year_end), xticks=range(self.year_start, self.year_end+1),
+        ylim=(self.age_start, self.age_end), yticks=range(self.age_start, self.age_end+1))
+
+        # grid
+        plt.grid()
+
+        for i in range(self.year_start, self.year_end):
+            plt.axline( (i, self.age_start), (i + 1  , self.age_start + 1 ), linewidth=0.3, color='gray')
+            plt.axline( (self.year_start, i - self.year_start), (self.year_start + 1, i - self.year_start + 1), linewidth=0.3, color='gray')
 
 
-    plt.show()
+        plt.show()
