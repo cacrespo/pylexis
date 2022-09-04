a74e187bfcd4989d54b9a4f151e417af31028c4f
Carlos A. Crespo <lvccrespo@gmail.com>
2022-09-04 19:34:06 -0300

    simplify setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
-import setuptools
+from setuptools import setup
 
-with open("requirements.txt", "rt") as fh:
-    install_requires = [x.strip() for x in fh]
 
-setuptools.setup(
+setup(
     name="pylexis",
     version="0.1.2",
     author="Carlos A. Crespo",
@@ -12,12 +10,13 @@ setuptools.setup(
     long_description=open("README.md", "r", encoding="utf8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/cacrespo/pylexis",
+    license="MIT",
+    packages=['pylexis'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=install_requires,
+    install_requires=["matplotlib"],
     python_requires=">=3.6",
-    packages=['pylexis']
 )
